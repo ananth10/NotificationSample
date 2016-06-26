54a992d42fdb40711f4d6383026fa07bfdb31af5
ananthababu <atom.ananthababu@gmail.com>
2016-06-26 13:33:23 +0530

    Some changes applied

--- app/src/main/java/com/ananth/notificationsample/MainActivity.java
+++ app/src/main/java/com/ananth/notificationsample/MainActivity.java
@@ -10,10 +10,13 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Build;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -24,6 +27,13 @@ import java.net.URL;
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
+    private TextView mCreateSimpleNotification;
+    private TextView mCreateStyleNotification;
+    private TextView mUpdateNotification;
+    private int numMessages = 0;
+    private NotificationCompat.Builder mNotifyBuilder;
+    private TextView mBigViewNotification;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,7 +45,39 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle("");
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.back_arrow);
-        new generatePictureStyleNotification(this,"MyNofi", "Hello", "http://api.androidhive.info/images/sample.jpg").execute();
+
+        mCreateSimpleNotification = (TextView) findViewById(R.id.create_simple_notification);
+        mCreateStyleNotification = (TextView) findViewById(R.id.create_style_notification);
+        mUpdateNotification = (TextView) findViewById(R.id.update_notification);
+        mBigViewNotification = (TextView) findViewById(R.id.big_view_notification);
+
+        mCreateSimpleNotification.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                simpleNotifcation();
+            }
+        });
+        mCreateStyleNotification.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new generatePictureStyleNotification(MainActivity.this, "MyNofi", "Hello", "http://cdn01.androidauthority.net/wp-content/uploads/2015/09/android-6.0-marshmallow.jpg").execute();
+            }
+        });
+
+        mUpdateNotification.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateNotifcation();
+            }
+        });
+
+        mBigViewNotification.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bigViewNotifcation();
+            }
+        });
+
     }
 
 
@@ -104,5 +146,68 @@ public class MainActivity extends AppCompatActivity {
             notif.flags |= Notification.FLAG_AUTO_CANCEL;
             notificationManager.notify(1, notif);
         }
+    }
+
+    private void simpleNotifcation() {
+        Intent intent = new Intent(MainActivity.this, MainActivity.class);
+        intent.putExtra("key", "value");
+        PendingIntent pendingIntent = PendingIntent.getActivity(MainActivity.this, 100, intent, PendingIntent.FLAG_ONE_SHOT);
+
+        NotificationManager notificationManager = (NotificationManager) MainActivity.this.getSystemService(Context.NOTIFICATION_SERVICE);
+        Notification notif = new Notification.Builder(MainActivity.this)
+                .setContentIntent(pendingIntent)
+                .setContentTitle("Simple Notification")
+                .setContentText("Happy Coding!!")
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .build();
+        notif.flags |= Notification.FLAG_AUTO_CANCEL;
+        notificationManager.notify(1, notif);
+    }
+
+    private void updateNotifcation() {
+        Intent intent = new Intent(MainActivity.this, MainActivity.class);
+        intent.putExtra("key", "value");
+        PendingIntent pendingIntent = PendingIntent.getActivity(MainActivity.this, 100, intent, PendingIntent.FLAG_ONE_SHOT);
+
+        NotificationManager notificationManager = (NotificationManager) MainActivity.this.getSystemService(Context.NOTIFICATION_SERVICE);
+        int notifyID = 1;
+        mNotifyBuilder = new NotificationCompat.Builder(this)
+                .setContentIntent(pendingIntent)
+                .setContentTitle("New Message")
+                .setContentText("You've received new messages.")
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentText("text").setNumber(++numMessages);
+        notificationManager.notify(
+                notifyID,
+                mNotifyBuilder.build());
+    }
+
+
+    private void bigViewNotifcation() {
+        Intent intent = new Intent(MainActivity.this, MainActivity.class);
+        intent.putExtra("key", "value");
+        PendingIntent pendingIntent = PendingIntent.getActivity(MainActivity.this, 100, intent, PendingIntent.FLAG_ONE_SHOT);
+
+
+        Intent dismissIntent = new Intent(this, MainActivity.class);
+        PendingIntent piDismiss = PendingIntent.getService(this, 0, dismissIntent, 0);
+
+        Intent snoozeIntent = new Intent(this, MainActivity.class);
+        PendingIntent piSnooze = PendingIntent.getService(this, 0, snoozeIntent, 0);
+        NotificationManager notificationManager = (NotificationManager) MainActivity.this.getSystemService(Context.NOTIFICATION_SERVICE);
+        mNotifyBuilder = new NotificationCompat.Builder(this)
+                .setContentIntent(pendingIntent)
+                .setContentTitle("New Message")
+                .setContentText("You've received new messages.")
+                .setStyle(new NotificationCompat.BigTextStyle()
+                        .bigText(getString(R.string.notification_msg)))
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .addAction(R.mipmap.dismiss,
+                        getString(R.string.dismiss), piDismiss)
+                .addAction(R.mipmap.snooze,
+                        getString(R.string.ok), piSnooze);
+        notificationManager.notify(
+                1,
+                mNotifyBuilder.build());
     }
 }
